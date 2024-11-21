c974408c7b827f8ac5e6f3221fb7c65afb84a602
Froggit2 <rus.yakovlev.03@gmail.com>
2024-11-21 11:14:21 +0300

    lol

--- Flask.py
+++ Flask.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, render_template
 import sqlite3
 
-connection = sqlite3.connect('Flask.db')
+
+connection = sqlite3.connect('Flask.db', check_same_thread=False)
 cursor = connection.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users
 (
-id INTEGER PRIMARY KEY,
-login TEXT,
-email TEXT,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+login TEXT UNIQUE,
 password TEXT
 )
 ''')
-
+connection.commit()
 
 app = Flask(__name__, template_folder='templates')
 
@@ -25,53 +25,46 @@ def glavn():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        login = request.form.get('login')
+        login_input = request.form.get('login')
         password = request.form.get('password')
-        email = request.form.get('email')
-        cursor.execute('SELECT login * FROM Users')
-        login_db = cursor.fetchall()
-        cursor.execute('SELECT password * FROM Users')
-        pass_db = cursor.fetchall()
-        cursor.execute('SELECT email * FROM Users')
-        email_db = cursor.fetchall()
-        if login == login_db and password == pass_db and email == email_db:
-            return render_template('login.html')
-        return render_template('login.html')
+        cursor.execute('SELECT * FROM Users WHERE login = ? AND password = ?',
+                       (login_input, password))
+        user = cursor.fetchone()
+        if user:
+            return render_template('login.html', message=f'Вы вошли как {login_input}')
+        else:
+            error = 'Неправильный логин или пароль'
+            return render_template('login.html', error=error)
     return render_template('login.html')
 
 
-@app.route('/logout')
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    pass
+    login_input = request.form.get('login')
+    password = request.form.get('password')
+    cursor.execute('DELETE FROM Users WHERE login = ? AND password = ?',
+                   (login_input, password))
+    connection.commit()
+    return render_template('logout.html')
 
 
 @app.route('/registr', methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
-        login = request.form.get('login')
-        password = request.form.get('password')
-        email = request.form.get('email')
-        cursor.execute('SELECT login * FROM Users')
-        login_db = cursor.fetchall()
-        if login_db == login:
+        login_input = request.form.get('login')
+        password_1 = request.form.get('password_1')
+        password_2 = request.form.get('password_2')
+        if password_1 != password_2:
+            error = 'Пароли должны совпадать!'
+            return render_template('registr.html', error=error)
+        cursor.execute('SELECT * FROM Users WHERE login = ?', (login_input,))
+        if cursor.fetchone():
             error = 'Логин не должен совпадать с логином другого пользователя!'
             return render_template('registr.html', error=error)
-        cursor.execute('SELECT email * FROM Users')
-        email_db = cursor.fetchall()
-        if email_db == email:
-            error = 'Нельзя зарегестрировать на одну почту два аккаунта!'
-            return render_template('registr.html', error=error)
-        # #
-        # cursor.execute('SELECT password * FROM Users')
-        # pass_db = cursor.fetchall()
-        # if pass_db != password:
-        #     error = 'Пароли должны совпадать!'
-        #     return render_template('registr.html', error=error)
-        # #
-        cursor.execute('INSERT INTO Users (login, email, password) VALUES(?, ?, ?)',
-                       (login, password))
+        cursor.execute('INSERT INTO Users (login, password) VALUES (?, ?)',
+                       (login_input, password_2))
         connection.commit()
-        return render_template('registr.html')
+        return render_template('registr.html', message='Регистрация прошла успешно!')
     return render_template('registr.html')
 
 
